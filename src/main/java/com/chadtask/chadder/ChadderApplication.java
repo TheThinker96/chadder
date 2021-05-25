554bfda6a132835ec2a452f9684afc24fa68f67a
TheThinker96 <loubensluma8@gmail.com>
2021-05-25 10:50:56 -0400

    First commit

---
+++ src/main/java/com/chadtask/chadder/ChadderApplication.java
@@ -0,0 +1,13 @@
+package com.chadtask.chadder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChadderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChadderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chadtask/chadder/ChadderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chadtask.chadder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChadderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
